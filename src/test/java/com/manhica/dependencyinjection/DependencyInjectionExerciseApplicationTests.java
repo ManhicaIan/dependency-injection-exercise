a90371d2796510795216644d9ae1621c61101d17
Ian Manhica <ianmanhica4@gmail.com>
2026-02-25 20:17:15 +0200

    Initial commit

---
+++ src/test/java/com/manhica/dependencyinjection/DependencyInjectionExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manhica.dependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
